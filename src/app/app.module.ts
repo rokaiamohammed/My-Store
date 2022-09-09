f6bb70895192699f878d82636213a251c2a0530d
rokaiamohammed <rokaia.mohamed111099@gmail.com>
2022-09-09 17:44:24 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ConfirmationPageComponent } from './confirmation-page/confirmation-page.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    ProductListComponent,
+    ConfirmationPageComponent,
+    ProductDetailsComponent,
+    CheckoutFormComponent
   ],
   imports: [
     BrowserModule,
