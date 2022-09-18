68aa423f7d219defdefbb18b4bd9197c7e11cfaf
rokaiamohammed <rokaia.mohamed111099@gmail.com>
2022-09-18 18:20:26 +0200

    add list of product as a service

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,24 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../Models/Product';
 import { ProductsService } from '../services/products.service';
 import { HttpClient } from '@angular/common/http';
+import { ListService } from '../services/list.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products:any;
+  products:Product[]=[];
 
   constructor(
     private productsService:ProductsService,
-    private http:HttpClient
+    private http:HttpClient,
+    private listService:ListService
     ) { }
 
   ngOnInit(): void {
-    this.http.get('assets/data.json').subscribe((res)=>{
-      this.products=res;
-    });
+    // this.http.get('assets/data.json').subscribe((res)=>{
+    //   this.products=res;
+    // });
     // this.productsService.resetProducts();
+    this.products=this.listService.getProducts();
   }
 
 
